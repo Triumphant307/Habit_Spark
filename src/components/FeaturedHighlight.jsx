475e6e98afe5d12444e64b1e8b77ca41e0a73b97
Triumphant307 <usuangbontriumphant@gmail.com>
2025-06-23 12:35:13 +0100

    Adds wellness and productivity tips to JSON data
    Introduces several new tips focused on wellness and productivity, enhancing the available advice for users.
    
    Also removes an unused import in the FeaturedHighlight component for cleaner code.

--- src/components/FeaturedHighlight.jsx
+++ src/components/FeaturedHighlight.jsx
@@ -1,5 +1,5 @@
 import styles from '../Styles/FeaturedHighlight.module.css';
-import { useEffect, useState } from 'react';
+import { useEffect,  } from 'react';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import { Link } from 'react-router-dom';
